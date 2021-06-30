37563f92330e47f5affc5e9bda13c4779cea9e8e
ncavchl <ncavchl@ajou.ac.kr>
2021-07-01 02:26:04 +0900

    mvc, api test

---
+++ hello-spring/src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,51 @@
+package com.hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("name", "hyerim");
+        model.addAttribute("data", "hello!");
+        return  "hello";
+//        hello html을 렌더링 해라 / thymeleaf 템플릿 엔진처리
+    }
+
+//   mvc 방식 test
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+
+//    json 방식으로 객체 출력
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+}
