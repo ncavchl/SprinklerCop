b84a662e45f0c8dce098b67267cfd5afced9b3ad
ncavchl <ncavchl@ajou.ac.kr>
2021-07-07 22:13:10 +0900

    testcase

--- hello-spring/src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
@@ -23,14 +23,17 @@ public class MemoryMemberRepository implements MemberRepository {
     }
 
     @Override
-    public Optional<Member> findByName(String name){
-        return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
     }
+
 
     @Override
     public List<Member> findAll(){
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
