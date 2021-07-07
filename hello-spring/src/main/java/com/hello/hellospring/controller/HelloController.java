50b25e00982cdd385b99c3ad90b7a4026ae42275
ncavchl <ncavchl@ajou.ac.kr>
2021-07-07 21:18:03 +0900

    00

--- hello-spring/src/main/java/com/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -38,7 +38,7 @@ public class HelloController {
         hello.setName(name);
         return hello;
     }
-//ffffff
+//fffff
     public class Hello {
         private String name;
         public String getName() {
