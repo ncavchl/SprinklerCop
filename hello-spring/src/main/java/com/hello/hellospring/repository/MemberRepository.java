eefe900d98165f7989f3d95cd40fa562e652eabb
ncavchl <ncavchl@ajou.ac.kr>
2021-07-07 21:34:22 +0900

    회원도메인, 레포지토리

--- hello-spring/src/main/java/com/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/com/hello/hellospring/repository/MemberRepository.java
@@ -6,7 +6,7 @@ import com.hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
-public class MemberRepository {
+public interface MemberRepository {
     Member save(Member member);
     //optional - null 이 올 경우에 optnional 로 감싸져서 받음
     Optional<Member> findById(Long id);
