5cd7a82e50555af27947dbe45174e17fb21a96df
ncavchl <ncavchl@ajou.ac.kr>
2021-08-17 00:58:19 +0900

    싱글톤 문제 해결, price받아서 처리후 반환

--- core/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,14 +15,14 @@ public class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean("statefulService",
                 StatefulService.class);
 //ThreadA: A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAprice = statefulService1.order("userA", 10000);
 //ThreadB: B사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBprice = statefulService2.order("userB", 20000);
 //ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
+//        int price = statefulService1.getPrice();
 //ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
-        System.out.println("price = " + price);
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        System.out.println("price = " + userAprice);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
     static class TestConfig {
         @Bean
